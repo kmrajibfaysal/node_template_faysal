309197b89e9dbe4e75e1bf8ee6420af7cc5ccf3d
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-01-29 20:34:53 +0600

    adding vscode settings file

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const faysal = 44;
-const rajib = 99;
-
-console.log(faysal + rajib);
+const fib = (n) => {
+    if (n === 1) {
+        return [0, 1];
+    }
+    const arr = fib(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
+};
