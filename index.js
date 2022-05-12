6841c1efc970c53027433687f0a2fc663bc98f66
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-05-12 06:12:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const fib = (n) => {
-    if (n === 1) {
-        return [0, 1];
-    }
-    const arr = fib(n - 1);
-    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-    return arr;
-};
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// Get requests
+app.get('/', (req, res) => {
+    res.send(`Doctors portal server running on port ${port}`);
+});
+// Listener
+app.listen(port, () => {
+    console.log(`Doctors portal server running on port ${port}`);
+});
